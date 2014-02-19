faf8f584408078807767da6a2fea49d3a050517c
任亚飞 <ren.yafee@gmail.com>
2014-02-19 10:24:40 +0800

    测试特殊语法
    结构体对象初始化语法，只在gcc下支持。

--- o_demo/provider/jni/three.c
+++ o_demo/provider/jni/three.c
@@ -1,1 +1,15 @@
 #include "one_api.h"
+
+typedef struct hmodule_t
+{
+	int (*open)(int* ptr);
+} hmodule_t;
+
+int hello_open(int* ptr)
+{	
+	return 0;
+}
+
+static struct hmodule_t hmodule_t_mothods = {
+	open : hello_open
+};
