2e5e92856d2782228f21e60b2d55a4ca05455658
任亚飞 <ren.yafee@gmail.com>
2014-06-06 16:52:55 +0800

    optim detail
    采用标准的gcc命令。

--- android_build_c_exec/gcc_app/run.cmd
+++ android_build_c_exec/gcc_app/run.cmd
@@ -1,1 +1,10 @@
-D:\android-ndk-r9b-windows-x86\toolchains\arm-linux-androideabi-4.6\prebuilt\windows\bin\arm-linux-androideabi-gcc.exe --sysroot=D:\android-ndk-r9b-windows-x86\platforms\android-13\arch-arm -o main_exec main.c
+@echo "1. build .o file"
+
+D:\android-ndk-r9b-windows-x86\toolchains\arm-linux-androideabi-4.6\prebuilt\windows\bin\arm-linux-androideabi-gcc.exe --sysroot=D:\android-ndk-r9b-windows-x86\platforms\android-13\arch-arm -c main.c
+
+@echo "2. build exec file"
+
+D:\android-ndk-r9b-windows-x86\toolchains\arm-linux-androideabi-4.6\prebuilt\windows\bin\arm-linux-androideabi-gcc.exe --sysroot=D:\android-ndk-r9b-windows-x86\platforms\android-13\arch-arm -o main_exec main.o
+
+@pause
+
